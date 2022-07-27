c297dc128cbc711048bce2934b0d16e4dc17660d
Ваня <undeadkivva@mail.ru>
2022-07-27 23:26:17 +0300

    Все исправил

--- src/MonthlyData.java
+++ src/MonthlyData.java
@@ -1,11 +1,11 @@
 import java.util.ArrayList;
 
 public  class MonthlyData {
-    public static ArrayList <MonthlyRecord> monthlyData;
+    public  ArrayList <MonthlyRecord> monthlyData;
     public MonthlyData(){
         monthlyData=new ArrayList<>();
     }
-    public static void addMonthlyData(MonthlyRecord record){
+    public  void addMonthlyData(MonthlyRecord record){
         monthlyData.add(record);
 
     }
